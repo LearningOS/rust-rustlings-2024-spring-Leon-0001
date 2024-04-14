d0ae0ec289fc6d854417b1b5526119a7a81295c6
Leon-0001 <1214642375@qq.com>
2024-04-14 10:58:25 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(apples:i32) -> i32 {
+
+    let costs = if apples <= 40 {
+        2 * apples
+    }
+    else {
+        apples
+    };
+    costs
+
+ }
 
 // Don't modify this function!
 #[test]
